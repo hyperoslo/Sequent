e347eac82cac42f23158da461273316469f2a106
Vadym Markov <markov.vadym@gmail.com>
2017-01-16 11:23:20 +0100

    Add example project

--- Example/SequentDemo/SequentDemo/Sources/State.swift
+++ Example/SequentDemo/SequentDemo/Sources/State.swift
@@ -1,9 +1,47 @@
-//
-//  State.swift
-//  SequentDemo
-//
-//  Created by Vadym Markov on 13/01/2017.
-//  Copyright © 2017 Hyper Interaktiv AS. All rights reserved.
-//
+import ReactiveReSwift
+import RxSwift
+import Sequent
+import Malibu
+import When
 
-import Foundation
+// State
+
+struct AppState: StateType {
+  var posts: Output<[String: Any]> = .data([:])
+}
+
+// Actions
+
+struct SetPostsAction: DynamicAction {
+  var payload: Output<[String: Any]>
+}
+
+struct FetchPosts: RequestActionCreator, GETRequestable {
+  typealias ActionType = SetPostsAction
+
+  let networking: String = "base"
+  var message: Message = Message(resource: "posts")
+
+  func transform(ride: Ride) -> Promise<[String: Any]> {
+    return ride.validate().toJsonDictionary()
+  }
+}
+
+// Reducer
+
+let reducer = Reducer<AppState> { action, state in
+  switch action {
+  case let setAction as SetPostsAction:
+    return AppState(posts: setAction.payload)
+  default:
+    return state
+  }
+}
+
+// Store
+
+let mainStore = Store(
+  reducer: reducer,
+  stateType: AppState.self,
+  observable: Variable(AppState())
+)
