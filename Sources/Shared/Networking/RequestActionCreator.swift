9823a0fcfdffa0794eaf7480d95ae5ac2accbcd3
Vadym Markov <markov.vadym@gmail.com>
2017-01-16 11:23:08 +0100

    Implement Malibu action creator

--- Sources/Shared/Networking/RequestActionCreator.swift
+++ Sources/Shared/Networking/RequestActionCreator.swift
@@ -1,9 +1,48 @@
-//
-//  RequestActionCreator.swift
-//  Pods
-//
-//  Created by Vadym Markov on 13/01/2017.
-//
-//
+import ReactiveReSwift
+import RxSwift
+import Malibu
+import When
 
-import Foundation
+public protocol RequestActionCreator: Requestable, ObservableConvertibleType {
+  associatedtype ActionType: DynamicAction
+  var networking: String { get }
+
+  func transform(ride: Ride) -> Promise<ActionType.DataType>
+  func buildAction(payload: Output<ActionType.DataType>) -> ActionType
+}
+
+public extension RequestActionCreator {
+
+  func buildAction(payload: Output<ActionType.DataType>) -> ActionType {
+    return ActionType(payload: payload)
+  }
+
+  func asObservable() -> Observable<ActionType> {
+    return Observable.create({ observer in
+      let ride = Malibu.networking(self.networking)
+        .execute(self)
+
+      self.transform(ride: ride)
+        .done({ data in
+          let action = self.buildAction(payload: Output.data(data))
+          observer.onNext(action)
+        })
+        .fail({ error in
+          observer.onNext(self.buildAction(payload: .error(error)))
+        })
+        .always({ _ in
+          observer.on(.completed)
+        })
+
+      return Disposables.create {
+        ride.cancel()
+      }
+    }).startWith(buildAction(payload: .progress))
+  }
+}
+
+public extension Store {
+  func dispatch<C: RequestActionCreator>(_ creator: C) where C.E: Action {
+    dispatch(creator.asObservable())
+  }
+}
